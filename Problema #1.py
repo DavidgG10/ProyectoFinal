b9116adbe07c8e61c619b9add2ff76ffd8cae85c
Isaacdavid11 <162249127+Isaacdavid11@users.noreply.github.com>
2024-03-04 20:21:53 -0600

    Add files via upload

---
+++ Problema #1.py
@@ -0,0 +1,15 @@
+# Numeros enteros 
+print("No se va a utlizar el numero 0!!")
+print ("")
+
+numero1=int(input("Ingrese el primer digito entero: "))
+numero2=int(input("Ingrese el segundo digito entero: "))
+
+division =float( numero1/numero2 )
+cubo1= numero1**3
+cubo2= numero2**3
+
+
+print("La division entre los numeros es: ",division)
+print("El cubo de",numero1,"es:",cubo1)
+print("El cubo de",numero2,"es:",cubo2)
