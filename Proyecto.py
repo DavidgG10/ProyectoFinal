27f3f3d59fb33f10f9e271b684bee0157b60d1ea
DavidG <davidgonzalezlda497@gmail.com>
2024-03-07 13:13:11 -0600

    Parte David

--- Proyecto.py
+++ Proyecto.py
@@ -1,1 +1,6 @@
 ###PROYECTO
+correo= input("Ingrese su correo electrónico:")
+Nombre_Co= input("Ingrese el nombre del comercio:")
+telefono= input("Digite el numero telefonico del comercio:")
+Nombre_Dueño= input("Ingrese su nombre:")
+Ubicacion== input("Ingrese la ubicación del local:")
