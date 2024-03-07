c6b4d236a5e2f77f824e129e001568c1165a0f32
DavidG <davidgonzalezlda497@gmail.com>
2024-03-07 17:28:59 -0600

    Parte David 2.0

--- Proyecto.py
+++ Proyecto.py
@@ -1,6 +1,10 @@
 ###PROYECTO
-correo= input("Ingrese su correo electrónico:")
-Nombre_Co= input("Ingrese el nombre del comercio:")
-telefono= input("Digite el numero telefonico del comercio:")
-Nombre_Dueño= input("Ingrese su nombre:")
-Ubicacion== input("Ingrese la ubicación del local:")
+def registro_de_usuario():
+    correo= input("Ingrese su correo electrónico:")
+    Nombre_Co= input("Ingrese el nombre del comercio:")
+    telefono= input("Digite el numero telefonico del comercio:")
+    Nombre_Dueño= input("Ingrese su nombre:")
+    Ubicacion = input("Ingrese la ubicación del local:")
+
+    print("Usuario registrado con exito")
+registro_de_usuario()
