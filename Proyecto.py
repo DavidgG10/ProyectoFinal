94cc95669d82782532f7ccd31e45986f8d7c88e8
Isaacdavid11 <isaacdmq@gmail.com>
2024-03-08 14:39:51 -0600

    Factura electrónica
    Mi parte junto ala de brayan.

--- Proyecto.py
+++ Proyecto.py
@@ -8,3 +8,16 @@ def registro_de_usuario():
 
     print("Usuario registrado con exito")
 registro_de_usuario()
+
+def registro_de_usuario():
+    tipo_cedula = input("Ingrese el tipo de cédula (Física o Jurídica): ")
+    num_cedula = input("Ingrese el número de cédula: ")
+    nombre = input("Ingrese el nombre registrado: ")
+    telefono = input("Ingrese el teléfono: ")
+    correo = input("Ingrese el correo electrónico: ")
+    provincia = input("Ingrese la provincia: ")
+    canton = input("Ingrese el cantón: ")
+    distrito = input("Ingrese el distrito: ")
+    
+    print("Usuario registrado con éxito")
+datos_usuario = registro_de_usuario()
